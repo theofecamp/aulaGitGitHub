df0e152042781bd843c0279c3cb040e2563c488e
Theo Fernandes <theofcampp@gmail.com>
2026-09-19 21:17:46 -0300

    Terceiro commit do projeto: apresentar o resultado da soma dos dois números.

--- arq.js
+++ arq.js
@@ -1,5 +1,5 @@
 function soma () {
-    var n1, n2;
+    var n1, n2, soma;
 
     n1 = document.getElementById ("primeiroNumero").value;
     n2 = document.getElementById ("segundoNumero").value;
@@ -8,4 +8,6 @@ function soma () {
     n2 = parseInt (n2);
 
     soma = n1 + n2;
+
+    document.getElementById ("resposta").innerHTML = soma;
 }
